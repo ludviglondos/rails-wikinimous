276d7b65cc4aad5ac37ba06b078c26a953ef0747
Ludvig Londos <ludvig.londos@gmail.com>
2019-06-03 21:32:01 +0200

    first commit

---
+++ db/migrate/20190603152119_add_number_name.rb
@@ -0,0 +1,5 @@
+class AddNumberName < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :number, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  article = Article.new(
+    title: Faker::Games::Pokemon.name,
+    content: Faker::Books::Lovecraft.sentence
+  )
+  article.save!
+end
